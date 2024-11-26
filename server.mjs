92114e2a37a2cfc24cfc2a1c63af2fe862450503
Stefan Lahtinen <stefan.lahtinen@gmail.com>
2024-11-26 12:19:31 +0100

    edit port

--- server.mjs
+++ server.mjs
@@ -25,6 +25,6 @@ app.use("/api/v1/requests", requestsRouter);
 
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5010;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
